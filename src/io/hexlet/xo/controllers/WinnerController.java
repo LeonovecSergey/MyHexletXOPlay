e052ac6567598f268e20215883ea3acbb0d40fb4
Сергей <sleonovec@gmail.com>
2017-09-11 23:17:05 +0300

    new version of the WinnerController (recursion)

--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -9,103 +9,52 @@ import java.awt.*;
 public class WinnerController {
 
     public Figure getWinner(final Field field){
+        try {
+            for (int i = 0; i < field.getSize(); i++){
+                if (check(field, new Point(i,0),point -> new Point(point.x, point.y + 1)))
+                    return field.getFigure(new Point(i,0));}
 
-        for (int i = 0; i > field.getSize(); i++){
-            if (checkHorizontalLine(field,i)) try {
-                return field.getFigure(new Point(i, 0));
-            } catch (InvalidPointException e) {
+            for (int i = 0; i < field.getSize(); i++){
+                if (check(field, new Point(0,i),point -> new Point(point.x + 1, point.y)))
+                    return field.getFigure(new Point(0,i));}
+
+            if (check(field, new Point(0,0),point -> new Point(point.x + 1, point.y + 1)))
+                return field.getFigure(new Point(0,0));
+
+            if (check(field, new Point(0,field.getSize() - 1),point -> new Point(point.x + 1, point.y - 1)))
+                return field.getFigure(new Point(0,field.getSize() - 1));
+
+            }catch (InvalidPointException e){
                 e.printStackTrace();
-            }
-        }
-
-        for (int i = 0; i > field.getSize(); i++){
-            if (checkVerticalLine(field,i)) try {
-                return field.getFigure(new Point(0, i));
-            } catch (InvalidPointException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (checkDiagonalUpDown(field)) try {
-            return field.getFigure(new Point(1, 1));
-        } catch (InvalidPointException e) {
-            e.printStackTrace();
-        }
-
-        if (checkDiagonalDownUp(field)) try {
-            return field.getFigure(new Point(1, 1));
-        } catch (InvalidPointException e) {
-            e.printStackTrace();
         }
 
         return null;
     }
 
-    private boolean checkHorizontalLine(final Field field, final int i){
-
-        try {if (field.getFigure(new Point(i, 0)) == null) return false;
-        }catch (InvalidPointException e){
-                e.printStackTrace();
-        }
+    private boolean check(final Field field, final Point currentPoint, final IPointGenerator pointGenerator){
+        final Figure currentFigure;
+        final Figure nextFigure;
+        final Point nextPoint = pointGenerator.next(currentPoint);
 
         try {
-            if (field.getFigure(new Point(i, 0)) == field.getFigure(new Point(i, 1)) &&
-                    field.getFigure(new Point(i, 0)) == field.getFigure(new Point(i, 2))) return true;
-        } catch (InvalidPointException e){
-            e.printStackTrace();
-        }
-        return false;
+            currentFigure = field.getFigure(currentPoint);
 
-        }
+            if (currentFigure == null) return false;
 
-    private boolean checkVerticalLine(final Field field, final int i){
-        try {if (field.getFigure(new Point(0, i)) == null) return false;
-        }catch (InvalidPointException e){
-            e.printStackTrace();
+            nextFigure = field.getFigure(nextPoint);
+        } catch (final InvalidPointException e) {
+            return true;
         }
 
-        try {
-            if (field.getFigure(new Point(0, i)) == field.getFigure(new Point(1, i)) &&
-                    field.getFigure(new Point(0, i)) == field.getFigure(new Point(2, i))) return true;
-        } catch (InvalidPointException e){
-            e.printStackTrace();
-        }
-        return false;
+        if (currentFigure != nextFigure) return false;
+
+        return check(field, nextPoint, pointGenerator);
     }
 
-    private boolean checkDiagonalUpDown(final Field field){
-        try {if (field.getFigure(new Point(0, 0)) == null) return false;
-        }catch (InvalidPointException e){
-            e.printStackTrace();
-        }
-        try {
-            if (field.getFigure(new Point(0, 0)) == field.getFigure(new Point(1, 1)) &&
-                    field.getFigure(new Point(0, 0)) == field.getFigure(new Point(2, 2))) return true;
-        } catch (InvalidPointException e){
-            e.printStackTrace();
-        }
-        return false;
-
+    private interface IPointGenerator{
+          Point next(final Point point);
     }
 
-    private boolean checkDiagonalDownUp(final Field field){
-        try {if (field.getFigure(new Point(2, 0)) == null) return false;
-        }catch (InvalidPointException e){
-            e.printStackTrace();
-        }
-        try {
-            if (field.getFigure(new Point(2, 0)) == field.getFigure(new Point(1, 1)) &&
-                    field.getFigure(new Point(2, 0)) == field.getFigure(new Point(0, 2))) return true;
-        } catch (InvalidPointException e){
-            e.printStackTrace();
-        }
-        return false;
-
-    }
-
-
-
-
-    }
+}
 
 
