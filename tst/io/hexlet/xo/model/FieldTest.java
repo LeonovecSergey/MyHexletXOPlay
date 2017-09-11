faa18e220420777943e8823e70ac917a4ae61f42
LeonovecSergey <sleonovec@gmail.com>
2017-09-11 15:41:56 +0300

    FieldTest - done

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -2,6 +2,8 @@ package io.hexlet.xo.model;
 
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class FieldTest {
@@ -15,6 +17,14 @@ public class FieldTest {
     @Test
     public void setFigure() throws Exception {
         final Field field = new Field();
+
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(inputFigure, actualFigure);
     }
 
 }
